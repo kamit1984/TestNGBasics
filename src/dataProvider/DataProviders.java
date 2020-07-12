c6236e2c590e5c0b860f6608c578e7559741396d
amit.kshirsagar <kamit1984@gmail.com>
2020-07-12 17:05:19 +0200

    Updated DataProviders.java file.

--- src/dataProvider/DataProviders.java
+++ src/dataProvider/DataProviders.java
@@ -10,32 +10,32 @@ public class DataProviders {
 	@DataProvider(name="dp1")
 	public static Object[][] getData(Method m){
 		
-		Object[][] data = null;
+		Object[][] stuff = null;
 		
 		if(m.getName().equals("testLogin")){
 		
-		data = new Object[2][2];
+		stuff = new Object[2][2];
 		
-		data[0][0] = "raman";
-		data[0][1] = "sdfdsfdilf";
+		stuff[0][0] = "raman";
+		stuff[0][1] = "sdfdsfdilf";
 		
-		data[1][0] = "Rahul";
-		data[1][1] = "asdfhsdlkfjls";
+		stuff[1][0] = "Rahul";
+		stuff[1][1] = "asdfhsdlkfjls";
 		
 		}else if(m.getName().equals("testUserReg")){
 			
-			data = new Object[2][3];
+			stuff = new Object[2][3];
 			
-			data[0][0] = "raman";
-			data[0][1] = "ahuja";
-			data[0][2] = "sdfds@gmail.com";
+			stuff[0][0] = "raman";
+			stuff[0][1] = "ahuja";
+			stuff[0][2] = "sdfds@gmail.com";
 			
-			data[1][0] = "Rahul";
-			data[1][1] = "Arora";
-			data[1][2] = "sdfdsfdilf@gmail.com";
+			stuff[1][0] = "Rahul";
+			stuff[1][1] = "Arora";
+			stuff[1][2] = "sdfdsfdilf@gmail.com";
 			
 		}
-		return data;
+		return stuff;
 		
 		
 	}
